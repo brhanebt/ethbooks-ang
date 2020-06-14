93512e08989fd99867cfd36985b46406448e77ef
brhanebt <33482700+brhanebt@users.noreply.github.com>
2020-06-14 10:37:29 -0500

    ethbooks frontend fix merge error heroku2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './services/book.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ethbooks-frontend';
+
+  categories: string[];
+
+  constructor(private bookService: BookService) { }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.categoryListService();
+  }
+
+  categoryListService() {
+    this.bookService.getBooksList().subscribe(
+      data => {
+        data.forEach(book => {
+          this.categories = {...this.categories, ...book.categories};
+        });
+    });
+  }
 }
